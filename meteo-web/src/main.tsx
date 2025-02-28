abbc14990c0c539bd2f10bb48eea918bbe522056
smazik02 <s.mazik2002@gmail.com>
2025-02-28 11:08:08 +0100

    fixed roboto font fetching

--- meteo-web/src/main.tsx
+++ meteo-web/src/main.tsx
@@ -3,8 +3,6 @@ import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router';
 import { router } from './router.tsx';
 
-import '@fontsource/roboto/400.css';
-
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <RouterProvider router={router} />
